c81c440c9e7db70642d1710d24ca30069c8892d0
nanmiao <nanmiaowu@gmail.com>
2019-10-10 10:58:41 -0500

    Change test matrix ;wq

--- tests/main_PreconditionedCG.cpp
+++ tests/main_PreconditionedCG.cpp
@@ -14,14 +14,21 @@ using namespace blaze::iterative;
 int main() {
 
     // Test Preconditioned CG
-    DynamicMatrix<double,false> A{{2, -1, 0},{-1, 2, -1}, {0, -1, 1}};
-    DynamicVector<double> b{0, 0, 1};
-    DynamicVector<double> x1{1, 2,3};
+    std::size_t N = 10;
+    DynamicMatrix<double,false> A(N,N, 0.0);
+    DynamicVector<double> b(N, 0.0);
+    DynamicVector<double> x1(N, 0.5);
+    for(int i=0; i<N; ++i) {
+        A(i,i) = 2.0;
+        b[i] = 1.0*(1+i);
+        x1[i] += x1[i-1];
+    }
+
 
     PreconditionCGTag tag;
     tag.do_log() = true;
-    auto x2 = solve(A,b,tag, "SSOR");
-
+    auto x2 = solve(A,b,tag, "incomplete_Cholesky");
+    
     auto error = norm(x1 - x2);
 
     if (error < EPSILON){
